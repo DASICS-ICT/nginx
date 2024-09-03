442e57c55dfa36afd0287359b064398779b6cc35
happynumb-wh <wanghan192@mails.ucas.ac.cn>
2024-09-03 14:13:44 +0800

    feat(openssl-libraray) Used openssl heartbleed version dynamic with x86 ISA to analyze backtrace

---
+++ nginx-x86.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+./auto/configure\
+ --with-cc-opt='-ggdb3 -O0'\
+ --prefix=$PWD/../nginx-bin-x86\
+ --builddir=$PWD/build\
+ --user=nginx\
+ --group=nginx\
+ --with-file-aio\
+ --with-ipv6\
+ --with-cc=gcc\
+ --with-cpp=g++\
+ --with-http_ssl_module\
+ --with-http_v2_module\
+ --with-http_realip_module\
+ --with-http_addition_module\
+ --with-http_sub_module\
+ --with-http_dav_module\
+ --with-http_flv_module\
+ --with-http_mp4_module\
+ --with-http_gunzip_module\
+ --with-http_gzip_static_module\
+ --with-http_random_index_module\
+ --with-http_secure_link_module\
+ --with-http_degradation_module\
+ --with-http_slice_module\
+ --with-http_auth_request_module\
+ --with-mail=dynamic\
+ --with-openssl-opt=yes\
+ --with-pcre\
+ --with-pcre-jit\
+ --with-stream=dynamic\
+ --with-stream_ssl_module\
+ --with-mail_ssl_module\
+ --with-debug\
+ --with-openssl=$PWD/../openssl\
+ --with-openssl-opt=
+#  --error-log-path=/var/log/nginx/error.log\
+#  --http-log-path=/var/log/nginx/access.log\
+#  --http-client-body-temp-path=/var/lib/nginx/tmp/client_body\
+#  --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi\
+#  --http-proxy-temp-path=/var/lib/nginx/tmp/proxy\
+#  --http-scgi-temp-path=/var/lib/nginx/tmp/scgi\
+#  --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi\
+#  --pid-path=/run/nginx.pid\
+#  --lock-path=/run/lock/subsys/nginx
+
+# make -j`nproc` && make install
